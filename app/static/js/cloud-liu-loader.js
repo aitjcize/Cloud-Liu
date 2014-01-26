7c9439aceaf7c30d0f1e0257fdcc81e7557918c3
AZ Huang <aitjcize@gmail.com>
2014-01-27 02:15:16 +0800

    Add GA code.

--- app/static/js/cloud-liu-loader.js
+++ app/static/js/cloud-liu-loader.js
@@ -63,6 +63,13 @@ require(['jquery-1.10.2.min'], function() {
     return;
   }
 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','cga');
+  cga('create', 'UA-47158795-2', document.domain);
+  cga('send', 'event', 'launch', document.URL);
+
   console.log('Cloud Liu: loading library ...');
   require([
     'jquery-textrange.min',
