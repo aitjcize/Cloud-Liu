b8c408ff0c2b95db4d012f4fd21bf0ed65fa7a8b
AZ Huang <aitjcize@gmail.com>
2014-01-26 23:06:06 +0800

    Remove query handler.

--- app/cloudliu.py
+++ app/cloudliu.py
@@ -12,38 +12,6 @@ database = join(dirname(realpath(__file__)), 'res/boshiamy.db')
 def index():
     return render_template("index.html", host=HOST)
 
-@app.route("/query.json", methods=['POST'])
-def query():
-    chs = request.form.getlist('keyStrokes[]', int)
-    query_str = 'SELECT phrase FROM phrases WHERE '
-
-    for j in range(5):
-        if j >= len(chs):
-            break
-        if chs[j] == 190: # '.'
-            alpha = '56'
-        elif chs[j] == 188: # ','
-            alpha = '55'
-        elif chs[j] == 222: # "'"
-            alpha = '27'
-        elif chs[j] == 219: # '['
-            alpha = '45'
-        elif chs[j] == 221: # ']'
-            alpha = '46'
-        else:
-            alpha = str(chs[j] - ord('A') + 1)
-        query_str += 'm%d=%s AND ' % (j, alpha)
-
-    query_str = query_str.strip(' AND ')
-    query_str += ' ORDER BY -freq LIMIT 10;'
-
-    conn = sqlite3.connect(database)
-    cursor = conn.cursor()
-    result = cursor.execute(query_str).fetchall()
-    print query_str
-
-    return jsonify({'candidates': [r[0] for r in result]})
-
 if __name__ == "__main__":
     app.debug = True
     HOST = "http://localhost:5000"
