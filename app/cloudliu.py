c212b2d6f810dc8760a619c6b7faa2b6e56634f1
AZ Huang <aitjcize@gmail.com>
2014-01-29 03:09:03 +0800

    Remove uneeded code.

--- app/cloudliu.py
+++ app/cloudliu.py
@@ -1,13 +1,9 @@
-import sqlite3
-
-from os.path import join, dirname, realpath
 from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
 HOST = "https://dl.dropboxusercontent.com/u/260733006"
 
-database = join(dirname(realpath(__file__)), 'res/boshiamy.db')
 @app.route("/")
 def index():
     return render_template("index.html", host=HOST)
